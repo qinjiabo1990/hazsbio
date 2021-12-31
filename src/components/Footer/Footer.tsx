2fd093da370fa83a5835574eecce8bb4ee7bd1aa
Bob Qin <bob.qin@tafeqld.edu.au>
2021-12-31 17:36:40 +1000

    set footer css

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,11 +5,11 @@ import styles from './Footer.module.css'
 export const Footer: React.FC = () => {
 	return (
 		<div className={styles.footer}>
-			<div>
+			<div className={styles.footerInfo}>
 				<p>Copyright © 2021 hazsbio.com All Rights Reserved</p>
 				<p>京ICP备 2020047285号-1 | 京公网安备00000000000号</p>
 			</div>
-			<img className={styles.img} src={weChat} alt="QR Code" />
+			<img className={styles.footerImg} src={weChat} alt="QR Code" />
 		</div>
 	)
 }
